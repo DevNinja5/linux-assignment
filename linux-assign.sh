be6c1b112a69b5d10b43e06c7f78ddd5cccb6ed8
Rahul Soni <rahul.soni@knoldus.com>
2021-08-23 22:54:59 +0530

    Added all exercises in a single file linux-assign.sh

--- linux-assign.sh
+++ linux-assign.sh
@@ -1,21 +1,36 @@
 #!/bin/sh
 
-#Exercise_1_2.
+#Exercise_1.
+echo "Shell scripting is FUN!"
+
+#Exercise_2.
 VAR="Shell scripting is FUN!"
 echo $VAR
 
 
 #Exercise_3.
-hostname=$(hostname -f)
-echo "The script is running on $hostname $(pwd) directory"
+HOSTNAME=$(hostname -f)
+echo "The script is running on $HOSTNAME in $(pwd) directory"
 
 #Exercise_4.
-bash animals.sh
+ANIMALS="man bear pig dog cat deer sheep"
+
+for MEMBER in $ANIMALS
+	do
+		echo $MEMBER
+	done
 
 #Exercise_5.
 echo "This script will exit with 0 exit status."
 echo "exit status: $(echo $?) "
 
 #Exercise_6.
+
+file_count(){
+	local COUNT=$(ls -l|grep -c '-')
+	echo $COUNT
+}
 echo "Total files in $(pwd) "
-bash count.sh file_count
+file_count
+
+
